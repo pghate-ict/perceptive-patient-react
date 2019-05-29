517f56914b08fe9912800998358b1e9ae653a06a
Pranav Ghate <pranavghate94@gmail.com>
2019-05-28 17:27:25 -0700

    Added Material UI Library

---
+++ src/js/components/presentational/NavigationBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Material UI
+
+// Navigation Component that allows the user to switch between different modules
+const NavigationBar = () => (
+    <div>
+        
+    </div>
+)
